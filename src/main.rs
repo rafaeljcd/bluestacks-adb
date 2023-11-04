6d003f9b838790ccca730caa7c7082d73ab125cb
Rafael <123484899+rafaeljcd@users.noreply.github.com>
2023-11-04 08:46:43 +0800

    Add error handling and improve output messages for adb connection

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use std::fs;
 use std::io::{BufRead, BufReader};
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 fn main() {
     let bluestacks_file = fs::File::open(r"C:\ProgramData\BlueStacks_nxt\bluestacks.conf").unwrap();
 
     let file = BufReader::new(bluestacks_file);
 
-    for line in file.lines() {
+    'outer: for line in file.lines() {
         for word in line.unwrap().split_whitespace() {
             if word.starts_with("bst.instance.Pie64.status.adb_port") {
                 let mut port = word.split('=');
                 run_adb_command(port.nth(1).unwrap().to_string().replace("\"", ""));
+                break 'outer;
             }
         }
     }
@@ -20,12 +21,14 @@ fn main() {
 fn run_adb_command(port: String) {
     let output = Command::new("adb")
         .args(&["connect", &format!("127.0.0.1:{}", port)])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .output()
         .expect("Unable to connect to adb");
 
-    if output.status.success(){
-        println!("Connected to {}", port);
-    } else {
+    if String::from_utf8_lossy(&output.stdout).starts_with("cannot connect") {
         println!("Unable to connect to {}", port);
+    } else {
+        println!("Connected to {0}", port);
     }
 }
